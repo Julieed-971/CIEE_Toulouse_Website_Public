4b06261a1b52efa45be55f967702223276cb468a
Julieed-971 <julie.dedieu1@gmail.com>
2024-07-12 18:42:59 +0200

    Added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,19 +4,19 @@ import logo from '../assets/images/logo.png';
 const Navbar = () => {
     
 
-return (
+    return (
         <nav className="navbar is-fixed-top" role="navigation" aria-label="main navigation" style={{ height: '30px' }}>
             <div className="navbar-menu">
             </div>
             <div className="navbar-start">
-					<div className="navbar-item">
-						<NavLink to="/" style={{ display: 'flex', alignItems: 'center' }}>
-								<img src={logo} alt="logo" style={{ width: 'auto', height: 'auto' }}/>
-						</NavLink>
-			    	</div>
-                    </div>
+				<div className="navbar-item">
+					<NavLink to="/" style={{ display: 'flex', alignItems: 'center' }}>
+						<img src={logo} alt="logo" style={{ width: 'auto', height: 'auto' }}/>
+					</NavLink>
+			    </div>
+            </div>
         </nav>   
-);
+    );
 };
 
 export default Navbar;
